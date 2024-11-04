d843a154642c714803d548672d37c3cf37585a3d
Apiário 2 Irmãos <111294338+tutianderson@users.noreply.github.com>
2024-11-04 10:07:01 -0300

    atualizei

--- graficos/quantidade de usuarios.js
+++ graficos/quantidade de usuarios.js
@@ -1,49 +1,33 @@
-async function quantidadeUsuarios() {
+import { getCSS } from "./common.js"
 
-}
-async function quantidadeUsuarios() {
-    const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero-usuarios.json'
-  }
-  async function quantidadeUsuarios() {
-    const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero-usuarios.json'
-    const res = await fetch(url)
-  }
-  async function quantidadeUsuarios() {
-    const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero-usuarios.json'
+async function quantidadeUsuariosPorRede() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
-  }
-  
-  quantidadeUsuarios()
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Redes Sociais</title>
-  <link rel="stylesheet" href="style.css">
-  <script src="https://cdn.plot.ly/plotly-2.27.0.min.js" charset="utf-8"></script>
-</head>
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Redes Sociais</title>
-  <link rel="stylesheet" href="style.css">
-  <script src="https://cdn.plot.ly/plotly-2.27.0.min.js" charset="utf-8"></script>
-</head>
-{"Facebook": 3049, "Youtube": 2491, "WhatsApp": 2000, "Instagram": 2000, "TikTok": 1562}
-async function quantidadeUsuarios() {
-  const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero-usuarios.json'
-  const res = await fetch(url)
-  const dados = await res.json()
-  const nomeDasRedes = Object.keys(dados)
-  const quantidadeUsuarios = Object.values(dados)
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeDeUsuarios = Object.values(dados)
 
+    const data = [
+        {
+            x: nomeDasRedes, 
+            y: quantidadeDeUsuarios, 
+            type: 'bar',
+            marker: {
+                color: getCSS('--primary-color')
+            }
+        }
+    ]
+
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color')
+
+    }
+
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data, layout)
 }
 
-quantidadeUsuarios()
-const data = [
-  {
-    x: nomeDasRedes,
-    y: quantidadeUsuarios,
-    type: 'bar'
-  }
-]
+quantidadeUsuariosPorRede()
